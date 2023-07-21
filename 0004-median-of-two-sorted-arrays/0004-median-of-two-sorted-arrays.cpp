39bb46a630bab65ee0910239c73e85563e937130
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-07-21 22:22:30 +0800

    Time: 48 ms (41.72%), Space: 89.3 MB (54.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
-    double findMedHelper(vector<int>& nums1, vector<int>& nums2, int target){
-        int n = nums1.size();
-        int m = nums2.size();
-        
-        int l = -1;
-        int r = m;
-        while(r-l>1){
-            int m = (l+r)/2;
-            int pos = (upper_bound(nums1.begin(), nums1.end(), nums2[m])-nums1.begin());
-            int smaller_cnt = pos + m+1;
-            if(smaller_cnt == target)
-                return nums2[m];
-            else if(smaller_cnt>target)
-                r = m;
-            else 
-                l = m;
-        }
-        
-        if(l==-1){
-            return nums1[target-1];
-        }
-        else{
-            int pos = (upper_bound(nums1.begin(), nums1.end(), nums2[l])-nums1.begin());
-            int smaller_cnt = pos + l+1;
-            int left = target - smaller_cnt;
-            return nums1[pos+left-1];
-        }
-    }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        if((m+n)%2!=0){
-            return findMedHelper(nums1, nums2, (m+n)/2+1);
-        }
-        else{
-            double a = findMedHelper(nums1, nums2, (m+n)/2);
-            double b = findMedHelper(nums1, nums2, (m+n)/2+1);
-            cout<<a<<" "<<b<<endl;
-            return (a+b)/2;
+        if(nums1.size() > nums2.size())
+            swap(nums1, nums2);
+        
+        int total = nums1.size() + nums2.size();
+        int half = total/2;
+        
+        int l = 0, r = nums1.size()-1;
+        while(true){
+            int m = floor((l+r)/2.0);
+            int left = half - (m+1) - 1;
+            
+            double nums1Left = (m>=0 ? nums1[m] : -INFINITY);
+            double nums1Right = (m<(int)nums1.size()-1 ? nums1[m+1] : INFINITY);
+            double nums2Left = (left>=0 ? nums2[left] : -INFINITY);
+            double nums2Right = (left<(int)nums2.size()-1 ? nums2[left+1] : INFINITY);
+            
+            if(nums1Left <= nums2Right && nums2Left <= nums1Right){
+                double leftVal = max(nums1Left, nums2Left);
+                double rightVal = min(nums1Right, nums2Right);
+                if(total%2==0)
+                    return (leftVal + rightVal)/2;
+                else
+                    return rightVal;
+            }
+            else if(nums1Left > nums2Right)
+                r = m-1; 
+            else
+                l = m+1;
         }
     }
 };
