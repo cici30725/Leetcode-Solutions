41898d63c98456ebad9707191231363a472cbe07
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-07-21 12:14:46 +0800

    Time: 4 ms (76.07%), Space: 11.9 MB (51.69%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int dfs(vector<int>& prices, int left, vector<int>& dp){
-        if(left>=prices.size())
+    int dfs(vector<int>& prices, int idx, int state, vector<vector<int>>& dp){
+        if(idx>=prices.size())
             return 0;
+        if(dp[idx][state]!=-1)
+            return dp[idx][state];
         
-        if(dp[left]!=-1)
-            return dp[left];
-        
-        int cur_min = prices[left];
-        int max_prof = 0;
-        
-        for(int i=left+1; i<prices.size(); i++){
-            cur_min = min(prices[i], cur_min);
-            max_prof = max(dfs(prices, i+2, dp) + (prices[i]-cur_min), max_prof);
+        if(state == 0){
+            // buy
+            int buy_profit = -prices[idx] + dfs(prices, idx+1, 1, dp);
+            int cool_profit = dfs(prices, idx+1, 0, dp);
+            return dp[idx][state] = max(buy_profit, cool_profit);
         }
-        
-        return dp[left] = max_prof;
+        else{
+            int sell_profit = prices[idx] + dfs(prices, idx+2, 0, dp);
+            int cool_profit = dfs(prices, idx+1, 1, dp);
+            return dp[idx][state] = max(sell_profit, cool_profit);
+        }
     }
     int maxProfit(vector<int>& prices) {
-        vector<int> dp(prices.size(), -1);
-        return dfs(prices, 0, dp);
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return dfs(prices, 0, 0, dp);
     }
 };
