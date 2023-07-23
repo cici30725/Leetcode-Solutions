5ffbe193696b35f721cadc206ce4cc0433a7f546
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-07-23 12:03:21 +0800

    Time: 28 ms (86.10%), Space: 44.5 MB (54.79%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -4,30 +4,19 @@ class Solution {
             return false;
         
         var M = new HashMap<Integer, Integer>();
-        var S = new TreeSet<Integer>();
+        for(int i : hand)
+            M.put(i, M.getOrDefault(i, 0)+1);
         
-        for(int i : hand){
-            int cur_val = 0;
-            if(M.containsKey(i))
-                cur_val = M.get(i);
-            if(cur_val == 0)
-                S.add(i);
-            M.put(i, cur_val+1);
-        }
+        Arrays.sort(hand);
         
-        while(!S.isEmpty()){
-            int cur_num = S.pollFirst();
-            int cur_cnt = M.get(cur_num);
-            if(cur_cnt==0)
+        for(int i=0; i<hand.length; i++){
+            if(M.get(hand[i])==0)     
                 continue;
-            for(int i = cur_num+1; i<cur_num+groupSize; i++){
-                if(!M.containsKey(i))
+            for(int j = 0; j<groupSize; j++){
+                int next = hand[i] + j;
+                if(M.getOrDefault(next, 0) == 0)
                     return false;
-                int next_cnt = M.get(i);
-                if(next_cnt < cur_cnt)
-                    return false;
-                else
-                    M.put(i, next_cnt - cur_cnt);
+                M.put(next, M.get(next)-1);
             }
         }
         return true;
