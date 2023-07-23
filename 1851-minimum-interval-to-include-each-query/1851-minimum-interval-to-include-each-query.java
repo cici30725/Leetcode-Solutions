62455b91e290f4a7d0f54540ee28e192468c1d6a
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-07-23 22:31:11 +0800

    Time: 120 ms (86.40%), Space: 104.1 MB (6.30%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -1,61 +1,24 @@
 class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
-        int[] queries_org = queries.clone();
-        Arrays.sort(intervals, new CustomComparator());
-        Arrays.sort(queries);
-        int[] sol = new int[queries.length];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> ((x[1]-x[0])-(y[1]-y[0])));
         var H = new HashMap<Integer, Integer>();
-        
-        int q_idx = 0;
-        int i_idx = 0;
-        var M = new TreeMap<Integer, Pair<Integer, Integer>>();
-        while(q_idx < queries.length){
-            while(i_idx < intervals.length && intervals[i_idx][0] <= queries[q_idx]){
-                int l = intervals[i_idx][0];
-                int r = intervals[i_idx][1];
-                int interval_len = r-l+1;
-                if(r >= queries[q_idx])
-                    M.put(interval_len, new Pair<Integer, Integer>(l, r));
-                i_idx++;
-            }
+        int[] arr = queries.clone();
+        Arrays.sort(arr);
+        Arrays.sort(intervals, (x, y) -> (x[0] - y[0])); 
+        int i = 0;
+        for(int q : arr){
+            while(i<intervals.length && intervals[i][0] <= q)
+                pq.offer(intervals[i++]);
             
-            while(!M.isEmpty()){
-                var entry = M.firstEntry();
-                int cur_interval_len = entry.getKey();
-                var cur_interval = entry.getValue();
-                int cur_l = cur_interval.getKey();
-                int cur_r = cur_interval.getValue();
-                
-                if(queries[q_idx]>=cur_l && queries[q_idx]<=cur_r){
-                    // sol[q_idx] = cur_interval_len;
-                    // H[queries[q_idx]] = cur_interval_len;
-                    H.put(queries[q_idx], cur_interval_len);
-                    break;
-                }
-                else
-                    M.pollFirstEntry();
-            }
+            while(!pq.isEmpty() && pq.peek()[1] < q)
+                pq.poll();
             
-            if(M.isEmpty()){
-                // sol[q_idx]=-1;
-                // H[queries[q_idx]] = -1;
-                H.put(queries[q_idx], -1);
-            }
-            
-            q_idx++;
+            H.put(q, pq.isEmpty() ? -1 : pq.peek()[1] - pq.peek()[0]+1);
         }
         
-        for(int i=0; i<queries_org.length; i++)
-            sol[i] = H.get(queries_org[i]);
-        return sol;
-    }
-    
-    // Custom comparator to compare based on the first entry of the subarrays
-    static class CustomComparator implements Comparator<int[]> {
-        @Override
-        public int compare(int[] a, int[] b) {
-            // Compare the first element of each subarray
-            return Integer.compare(a[0], b[0]);
-        }
+        for(int j=0; j<queries.length; j++)
+            queries[j] = H.get(queries[j]);
+        
+        return queries;
     }
 }
