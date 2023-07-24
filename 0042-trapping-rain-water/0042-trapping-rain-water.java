d7d238745904499dd610b4faa3fc2512d0126f3e
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-07-24 10:29:25 +0800

    Time: 1 ms (95.51%), Space: 44.3 MB (76.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,18 @@
 class Solution {
     public int trap(int[] height) {
-        if(height.length<=2)
-            return 0;
-        var pq = new PriorityQueue<int[]>((x, y) -> (y[0]-x[0]));
-        for(int i=height.length-1; i>1; i--)
-            pq.offer(new int[]{height[i], i});
-        
-        int l_max = height[0];
-        int sol = 0;
-        for(int i=1; i<height.length-1; i++){
-            while(pq.peek()[1] <= i)
-                pq.poll();
-            int r_max = pq.peek()[0];
-            int k = Math.min(l_max, r_max);
-            int added = Math.max(0, k - height[i]);
-            // System.out.format("%d %d %d %d\n", i, l_max, r_max, added);
-            sol += added;
-            l_max = Math.max(l_max, height[i]);
-            // pq.remove({height[i], i});
+        int sol = 0, l = 0, r = height.length-1;
+        int lMax = height[l], rMax = height[r];
+        while(l<r){
+            if(lMax<rMax){
+                l++;
+                lMax = Math.max(lMax, height[l]);
+                sol += lMax-height[l];
+            }
+            else{
+                r--;
+                rMax = Math.max(rMax, height[r]);
+                sol += rMax-height[r];
+            }
         }
         return sol;
     }
