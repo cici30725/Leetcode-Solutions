ef11bb951dbdd6e35d57ca6a28fa8c4a2adc8396
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-06 22:32:43 +0800

    Time: 17 ms (34.22%), Space: 14.4 MB (14.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> M;
-        for(int i : nums){
-            M[i]++;
+        int n = nums.size();
+        unordered_map<int, int> freq_m;
+        for(int i : nums)
+            freq_m[i]+=1;
+        
+        vector<int> arr(n+1, 0); // arr[i] = # distinct ele with i freq
+        for(auto& [k, v] : freq_m){
+            arr[v] += 1;
         }
         
-        priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
-        for(auto& [key, val] : M){
-            pq.push({val, key});
+        vector<int> cum(n+1, 0); // cum[i] = # elements with >=i freq
+        cum[n] = arr[n];
+        for(int i=n-1; i>=1; i--){
+            cum[i] = cum[i+1] + arr[i];
         }
         
+        
+        int l = 0, r = n+1;
+        while(r-l>1){
+            int m = (l+r)/2;
+            if(cum[m]>=k){
+                l = m;
+            }
+            else{
+                r = m;
+            }
+        }
+        
+        // lowest freq is l
         vector<int> sol;
-        for(int i=0; i<k; i++){
-            sol.push_back(pq.top().second);
-            pq.pop();
+        for(auto& [k, v] : freq_m){
+            if(v >= l)
+                sol.push_back(k);
         }
         return sol;
     }
