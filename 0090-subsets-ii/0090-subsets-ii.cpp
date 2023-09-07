47663d2283fe16a9c56fb5760093264d0702d89e
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-07 11:25:31 +0800

    Time: 0 ms (100.00%), Space: 7.5 MB (63.75%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    
-    void dfs(int idx, vector<int>& nums, vector<int>& cur_sol, vector<vector<int>>& sol){
+    void dfs(vector<vector<int>>& sol, vector<int>& nums, vector<int>& cur_arr, int idx){
         if(idx == nums.size()){
-            sol.push_back(cur_sol);
+            sol.push_back(cur_arr);
             return;
         }
-        
-        cur_sol.push_back(nums[idx]);
-        dfs(idx+1, nums, cur_sol, sol);
-        cur_sol.pop_back();
-        
-        while(idx+1 < nums.size() && nums[idx+1]==nums[idx])
-            idx+=1;
-        dfs(idx+1, nums, cur_sol, sol);
+        cur_arr.push_back(nums[idx]);
+        dfs(sol, nums, cur_arr, idx+1);
+        cur_arr.pop_back();
+        int i = idx+1;
+        while(i<nums.size() && nums[i]==nums[i-1])
+            i++;
+        dfs(sol, nums, cur_arr, i);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<int> cur_sol;
         vector<vector<int>> sol;
-        dfs(0, nums, cur_sol, sol);
+        vector<int> cur_arr;
+        dfs(sol, nums, cur_arr, 0);
         return sol;
     }
 };
