cc3e3debdaaf0464b8eb4cd58c117e99c6587f4d
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-07 10:43:17 +0800

    Time: 4 ms (41.79%), Space: 7.6 MB (83.59%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> sol;
-        do {
+    void dfs(vector<vector<int>>& sol, int cur, vector<int>& nums) {
+        if(cur == nums.size()){
             sol.push_back(nums);
-        } while(next_permutation(nums.begin(), nums.end()));
+            return;
+        }
+        for(int i=cur; i<nums.size(); i++){
+            swap(nums[i], nums[cur]);
+            dfs(sol, cur+1, nums);
+            swap(nums[i], nums[cur]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> sol;
+        dfs(sol, 0, nums);
         return sol;
     }
 };
