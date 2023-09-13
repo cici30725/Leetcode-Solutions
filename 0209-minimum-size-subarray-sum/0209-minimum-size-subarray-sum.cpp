20ce0bb41b77a7b740dc059a09c4a088c8584996
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-13 17:48:30 +0800

    Time: 29 ms (64.34%), Space: 28.6 MB (6.20%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -3,19 +3,15 @@ public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int l = 0, r = 0;
         int sol = INT_MAX;
-        int cur = nums[0];
+        int cur = 0;
         while(r < nums.size()){
-            // cout<<l<<" "<<r<<" "<<cur<<endl;
-            if(cur >= target){
+            cur+=nums[r];
+            while(cur>=target){
                 sol = min(sol, r-l+1);
                 cur-=nums[l];
                 l++;
             }
-            else{
-                r++;
-                if(r < nums.size())
-                    cur+=nums[r];
-            }
+            r++;
         }
         
         return sol==INT_MAX ? 0 : sol;
