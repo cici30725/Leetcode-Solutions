6c8d34a13b2bc998e534c7e245752c8a0c23e2af
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-13 22:01:45 +0800

    Time: 107 ms (91.85%), Space: 71.6 MB (26.70%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int type_a = fruits[0], type_b = -1;
-        int a_idx = 0, b_idx = -1;
-        int cnt = 1;
-        int sol = 1;
-        for(int i=1; i<fruits.size(); i++){
-            /*
-            if(fruits[i]!=type_a && type_b == -1){
-                type_b = fruits[i];
-                b_idx = i;
-                cnt++;
-            }
-            */
-            if(fruits[i]!=type_a && fruits[i]!=type_b){
-                if(b_idx < a_idx){
-                    cnt = i-b_idx;
-                    b_idx = i;
-                    type_b = fruits[i];
+        unordered_map<int, int> M;
+        int cur = 0;
+        int sol = 0;
+        for(int i=0; i<fruits.size(); i++){
+            M[fruits[i]] = i;
+            if(M.size()>2){
+                int out;
+                for(auto& [type, _] : M){
+                    if(type!=fruits[i] && type!=fruits[i-1]){
+                        out = type;
+                        break;
+                    }
                 }
-                else{
-                    cnt = i - a_idx;
-                    a_idx = i;
-                    type_a = fruits[i];
-                }
+                cur = i - M[out];
+                M.erase(out);
             }
-            else if(fruits[i]==type_a){
-                a_idx = i;
-                cnt++;
-            }
-            else{
-                b_idx = i;
-                cnt++;
-            }
-            sol = max(cnt, sol);
+            else
+                cur++;
+            sol = max(sol, cur);
         }
         return sol;
     }
