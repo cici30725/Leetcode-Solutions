6b96b4c5662a7811ae45a872e26b9a9a5e725dff
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-16 00:06:32 +0800

    Time: 0 ms (100.00%), Space: 7.1 MB (13.36%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        vector<unsigned> dp(target+1, 0);
+        vector<unsigned int> dp(target+1, 0);
         dp[0] = 1;
         for(int cur = 1; cur<=target; cur++){
             for(int i : nums){
