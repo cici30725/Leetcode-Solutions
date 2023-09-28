9494527284ceac5cb9a7d2103ec5abfbc1851cfb
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-28 20:38:04 +0800

    Time: 16 ms (30.46%), Space: 8.6 MB (25.06%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> T = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int res = T[s.back()];
+        for(int i=s.size()-2; i>=0; i--){
+            if(T[s[i]] < T[s[i+1]]){
+                res-=T[s[i]];
+            }
+            else{
+                res+=T[s[i]];
+            }
+        }
+        
+        return res;
+    }
+};
