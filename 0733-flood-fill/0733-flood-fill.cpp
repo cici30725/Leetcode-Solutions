c7fe3fcc2a5a53eff3ed13a9147f7da8513c6f80
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-28 16:28:48 +0800

    Time: 4 ms (88.16%), Space: 14.2 MB (37.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size(), n = image[0].size();
-        
-        vector<vector<int>> visit(m, vector<int>(n, false));
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-        
-        vector<pair<int, int>> dir = {
-            {1, 0},
-            {0, 1},
-            {-1, 0},
-            {0, -1}
-        };
-        
-        int org_color = image[sr][sc];
-        
-        while(!q.empty()){
-            auto [cur_r, cur_c] = q.front();
-            q.pop();
-            
-            if(visit[cur_r][cur_c])
-                continue;
-            
-            visit[cur_r][cur_c] = true;
-            image[cur_r][cur_c] = color;
-            for(auto& [dr, dc]: dir){
-                int next_r = cur_r + dr, next_c = cur_c + dc;
-                if(0<=next_r && next_r < m && 0<=next_c && next_c < n && image[next_r][next_c] == org_color){
-                    q.push({next_r, next_c});
-                }
-            }
+    void dfs(vector<vector<int>>& image, int r, int c, int orgColor, int color){
+        if(image[r][c] == orgColor){
+            image[r][c] = color;
+            if(r>=1) dfs(image, r-1, c, orgColor, color);
+            if(r<image.size()-1) dfs(image, r+1, c, orgColor, color);
+            if(c>=1) dfs(image, r, c-1, orgColor, color);
+            if(c < image[0].size()-1) dfs(image, r, c+1, orgColor, color);
         }
-        
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int orgColor = image[sr][sc];
+        if(orgColor != color) dfs(image, sr, sc, orgColor, color);
         return image;
     }
 };
