23e78cf284b23967951ab018755aafd2e6f9cc63
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-09-29 09:25:21 +0800

    Time: 14 ms (54.26%), Space: 17.5 MB (20.26%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -3,31 +3,22 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> sol;
         int n = intervals.size();
-        if(n==0)
-            return {newInterval};
         
-        int left_i = 0;
-        while(left_i < n && intervals[left_i][1] < newInterval[0])
-            left_i++;
-        
-        int right_i = n-1;
-        while(right_i >= 0 && intervals[right_i][0] > newInterval[1])
-            right_i--;
-        
-        for(int i=0; i<left_i; i++)
-            sol.push_back(intervals[i]);
-        
-        int cur_l = newInterval[0], cur_r = newInterval[1];
-        for(int i=left_i; i<=right_i; i++){
-            cur_l = min(cur_l, intervals[i][0]);
-            cur_r = max(cur_r, intervals[i][1]);
+        int i=0;
+        while(i<n){
+            if(intervals[i][1] < newInterval[0])
+                sol.push_back(intervals[i]);
+            else if(intervals[i][0] > newInterval[1])
+                break;
+            else{
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+            i++;
         }
         
-        sol.push_back({cur_l, cur_r});
-        
-        for(int i=right_i+1; i<n; i++)
-            sol.push_back(intervals[i]);
-        
+        sol.push_back(newInterval);
+        sol.insert(sol.end(), intervals.begin()+i, intervals.end());
         return sol;
     }
 };
