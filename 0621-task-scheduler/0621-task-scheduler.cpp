8827f4e2acebb5ff1658f9a35e37b284d0620592
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-10-03 09:08:14 +0800

    Time: 52 ms (76.36%), Space: 34.7 MB (64.23%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        array<int, 26> freq{};
-        for(char c : tasks){
+        vector<int> freq(26, 0);
+        for(char c : tasks)
             freq[c-'A']++;
+        
+        sort(freq.begin(), freq.end());
+        
+        int max_f = freq[freq.size()-1];
+        int idle_slots = (max_f-1) * n;
+        for(int i=freq.size()-2; i>=0; i--){
+            idle_slots -= min(max_f-1, freq[i]) ;
         }
         
-        priority_queue<pair<int, int>, vector<pair<int, int>>> pq; // freq, task
-        queue<pair<int, int>> q; // time, task
-        for(int i=0; i<26; i++){
-            if(freq[i] > 0)
-                pq.push({freq[i], i});
-        }
-        
-        int cur_time = 0;
-        while(!pq.empty() || !q.empty()){
-            while(!q.empty() && q.front().first <= cur_time){
-                auto [time, task]  = q.front();
-                pq.push({freq[task], task});
-                q.pop();
-            }
-            
-            if(!pq.empty()){
-                auto [cur_freq, task] = pq.top();
-                pq.pop();
-                if(--freq[task] > 0)
-                    q.push({cur_time + n + 1, task});
-            }
-            cur_time++;
-        }
-        
-        return cur_time;
+        return idle_slots < 0 ? tasks.size() : tasks.size() + idle_slots;
     }
 };
