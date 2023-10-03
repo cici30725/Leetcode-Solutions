4eb640f1a36b397f391aee5badcc08d845a83d5f
cici30725 <45911542+cici30725@users.noreply.github.com>
2023-10-03 09:35:35 +0800

    Time: 362 ms (85.04%), Space: 165.5 MB (71.51%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,39 +1,37 @@
 class LRUCache {
 public:
-    int cap;
-    using pii = pair<int, int>;
-    unordered_map<int, list<pii>::iterator> m_map;
-    list<pii> m_list;
-    
-    LRUCache(int capacity) {
-        cap = capacity;
+    list<pair<int, int>> data;
+    unordered_map<int, list<pair<int, int>>::iterator> cache;
+    int m_cap;
+        
+    LRUCache(int capacity): m_cap{capacity} {
+        
     }
     
     int get(int key) {
-        auto it = m_map.find(key);
-        if(it==m_map.end())
+        if(!cache.count(key))
             return -1;
-        
-        m_list.splice(m_list.begin(), m_list, it->second);
-        return it->second->second;
+        auto& it = cache[key];
+        data.splice(data.begin(), data, it);
+        return data.begin()->second;
     }
     
     void put(int key, int value) {
-        auto it = m_map.find(key);
-        if(it!=m_map.end()){
-            it->second->second = value;
-            m_list.splice(m_list.begin(), m_list, it->second);
+        if(cache.count(key)){
+            auto& it = cache[key];
+            it->second = value;
+            data.splice(data.begin(), data, it);
             return;
         }
+            
+        data.push_front({key, value});
+        cache[key] = data.begin();
         
-        if(m_map.size() == cap){
-            int key_to_remove = m_list.back().first;
-            m_list.pop_back();
-            m_map.erase(key_to_remove);
+        if(data.size() > m_cap){
+            int removeKey = data.back().first;
+            data.pop_back();
+            cache.erase(removeKey);
         }
-        
-        m_list.emplace_front(key, value);
-        m_map[key] = m_list.begin();
     }
 };
 
